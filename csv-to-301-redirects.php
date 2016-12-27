425164df1a05746b26fcbdaedf0157435750edd7
tmdk <tobiasvandijk@gmail.com>
2016-12-27 16:34:51 +0100

    Check CSV file readability before determining mime type

--- csv-to-301-redirects.php
+++ csv-to-301-redirects.php
@@ -35,24 +35,24 @@ License URI: http://www.gnu.org/licenses/gpl-2.0.html
  * @since 1.0.0
  */
 function csv_to_301_redirects() {
-			
+
 	// Variables
 	$uri		= $_SERVER['REQUEST_URI'];
 	$file_path	= csv_to_301_redirects_get_file();
 	$file 		= csv_to_301_redirects_check_file( $file_path );
 	$redirects	= csv_to_301_redirects_get_redirects( $file );
-			
+
 	// Checks
-	if ( false !== $file_path && false !== $file && false !== $redirects && true === isset( $redirects[ $uri ] ) ) { 
-						
+	if ( false !== $file_path && false !== $file && false !== $redirects && true === isset( $redirects[ $uri ] ) ) {
+
 		// Redirect
 		wp_redirect( $redirects[ $uri ], 301 );
 
 		// Exit
 		exit;
-		
+
 	}
-		
+
 }
 add_action( 'init', 'csv_to_301_redirects' );
 
@@ -64,26 +64,26 @@ add_action( 'init', 'csv_to_301_redirects' );
  * @return array, bool	string with .csv file path
  */
 function csv_to_301_redirects_get_file() {
-	
+
 	// Check
 	if ( false !== get_option( 'csv_to_301_redirects_csv_file_path' ) ) {
-	
+
 		// Get attachment ID
 		$attachment_id = (int) get_option( 'csv_to_301_redirects_csv_file_path' )['csv_to_301_redirects_csv_file_path'];
-				
+
 		// Get file path
 		$file_path = get_attached_file( $attachment_id, false );
 
 		// Return
 		return $file_path;
-			
+
 	} else {
-		
+
 		// Return
 		return false;
-		
-	} 
-	
+
+	}
+
 }
 
 
@@ -91,10 +91,10 @@ function csv_to_301_redirects_get_file() {
  * Checks the given attachment file type
  *
  * @since 1.0.0
- * @return array, bool	string with .csv file path or false on error
+ * @return string|bool	string with .csv file path or false on error
  */
 function csv_to_301_redirects_check_file( $file_path = false ) {
-	
+
 	// Variables
 	$mime_types = array(
 		'application/csv',
@@ -114,20 +114,17 @@ function csv_to_301_redirects_check_file( $file_path = false ) {
 		'text/plain',
 		'text/comma-separated-values'
 	);
-	
-	// Check mime type
-	if( false !== $file_path && false !== in_array( mime_content_type( $file_path ), $mime_types ) ) {
-		
-		// Return
-		return $file_path;
-		
-	} else {
-		
-		// Return
+
+	if ( $file_path === false || ! is_readable( $file_path ) ) {
 		return false;
-		
 	}
-	
+
+	// Check mime type
+	if ( in_array( mime_content_type( $file_path ), $mime_types ) ) {
+		return $file_path;
+	} else {
+		return false;
+	}
 }
 
 
@@ -138,13 +135,13 @@ function csv_to_301_redirects_check_file( $file_path = false ) {
  * @return array, bool	array with redirects or boolean if no redirects found.
  */
 function csv_to_301_redirects_get_redirects( $file = false ) {
-	
+
 	// Check
-	if ( false !== $file ) { 
-	
+	if ( false !== $file ) {
+
 		// Variables
 		$redirects = [];
-		
+
 		// Open
 		$handle = fopen( $file, 'r' );
 
@@ -157,18 +154,18 @@ function csv_to_301_redirects_get_redirects( $file = false ) {
 		}
 
 		// Close
-		fclose( $handle );	
+		fclose( $handle );
 
 		// Return
 		return $redirects;
-		
+
 	} else {
-		
+
 		// Return
 		return false;
-		
-	} 
-		
+
+	}
+
 }
 
 
@@ -178,13 +175,13 @@ function csv_to_301_redirects_get_redirects( $file = false ) {
  * @since 1.0.0
  */
 function csv_to_301_redirects_settings_init() {
-    
+
 	// Register setting
     register_setting( 'csv-to-301-redirects', 'csv_to_301_redirects_csv_file_path' );
- 
+
     // Add section
     add_settings_section( 'csv_to_301_redirects_section_developers', null, null, 'csv-to-301-redirects' );
- 
+
     // Register field
     add_settings_field(
         'csv_to_301_redirects_csv_file_path',
@@ -198,7 +195,7 @@ function csv_to_301_redirects_settings_init() {
 			'csv_to_301_redirects_custom_data' => 'custom',
         ]
     );
-	
+
 }
 add_action( 'admin_init', 'csv_to_301_redirects_settings_init' );
 
@@ -209,7 +206,7 @@ add_action( 'admin_init', 'csv_to_301_redirects_settings_init' );
  * @since 1.0.0
  */
 function csv_to_301_redirects_csv_file_path_callback( $args ) {
-   		
+
 	// Output
     ?>
     <input type="number"
@@ -218,7 +215,7 @@ function csv_to_301_redirects_csv_file_path_callback( $args ) {
            name="csv_to_301_redirects_csv_file_path[<?php echo esc_attr( $args['label_for'] ); ?>]"
 		   value="<?php echo esc_attr( ( get_option( 'csv_to_301_redirects_csv_file_path' )['csv_to_301_redirects_csv_file_path'] ) ? get_option( 'csv_to_301_redirects_csv_file_path' )['csv_to_301_redirects_csv_file_path'] : ('') ) ; ?>" />
     <?php
-	
+
 }
 
 
@@ -228,7 +225,7 @@ function csv_to_301_redirects_csv_file_path_callback( $args ) {
  * @since 1.0.0
  */
 function csv_to_301_redirects_options_page() {
-	
+
     // Add option pages
     add_options_page(
         'CSV to 301 Redirects',
@@ -237,13 +234,13 @@ function csv_to_301_redirects_options_page() {
 		'csv-to-301-redirects-options',
         'csv_to_301_redirects_options_page_html'
     );
-	
+
 	// Remove update nag
 	remove_action( 'admin_notices', 'update_nag', 3 );
-	
+
 }
 add_action( 'admin_menu', 'csv_to_301_redirects_options_page' );
- 
+
 
 /**
  * Outputs HTML on the options page
@@ -251,44 +248,53 @@ add_action( 'admin_menu', 'csv_to_301_redirects_options_page' );
  * @since 1.0.0
  */
 function csv_to_301_redirects_options_page_html() {
-    
+
 	// Check capabilities
     if ( !current_user_can( 'manage_options' ) ) {
         return;
     }
-	
+
 	// Variables
-	$file_path	= csv_to_301_redirects_get_file();
-	$file 		= csv_to_301_redirects_check_file( $file_path );
-	$redirects	= csv_to_301_redirects_get_redirects( $file );
-	
+	$file_path = csv_to_301_redirects_get_file();
+	$file      = csv_to_301_redirects_check_file( $file_path );
+	$redirects = csv_to_301_redirects_get_redirects( $file );
+
 	// Check file_path
 	if ( false === $file_path ) {
-		
+
 		// Show file error message
 		add_settings_error( 'csv_to_301_redirects_messages', 'csv_to_301_redirects_message', __( 'Upload a .csv file in the <a href="' . esc_url( get_admin_url() . 'upload.php' ) . '" target="_blank" title="Open the WordPress Media Library">media library</a> and enter the attachment ID below. Make sure your .csv is formatted correctly (see the example .csv files in the plugin directory).', 'csv-to-301-redirects' ), 'notice-info' );
-		
+
 	}
-	
+
 	// Check file
 	if ( false === $file && false !== $file_path ) {
-		
-		// Show MIME error message
-		add_settings_error( 'csv_to_301_redirects_messages', 'csv_to_301_redirects_message', __( 'Attachment has an invalid mime type. Create a message in the plugin support forum if you\'re 100% sure you\'ve uploaded a genuine .csv file', 'csv-to-301-redirects' ), 'notice-error' );
-		
+
+		if ( is_readable( $file_path ) ) {
+
+			// Show MIME error message
+			add_settings_error( 'csv_to_301_redirects_messages', 'csv_to_301_redirects_message', __( 'Attachment has an invalid mime type. Create a message in the plugin support forum if you\'re 100% sure you\'ve uploaded a genuine .csv file', 'csv-to-301-redirects' ), 'notice-error' );
+
+		} else {
+
+			// Show file unreadable error message
+			add_settings_error( 'csv_to_301_redirects_messages', 'csv_to_301_redirects_message', __( 'Cannot read uploaded CSV file. Check your upload settings and try reuploading the file.', 'csv-to-301-redirects' ), 'notice-error' );
+
+		}
+
 	}
-	
+
 	// Nofity of redirects
 	if ( false !== $file_path && false !== $file && false !== $redirects ) {
-		
+
 		// Show redirecting message
 		add_settings_error( 'csv_to_301_redirects_messages', 'csv_to_301_redirects_message', __( 'Redirects active. Please confirm by clicking some of the sources below. Clearing the attachment ID will stop all redirects from working.', 'csv-to-301-redirects' ), 'notice-success' );
-		
+
 	}
-	
+
     // Show message
     settings_errors( 'csv_to_301_redirects_messages' );
-	
+
 	// Output
     ?>
 
@@ -299,16 +305,16 @@ function csv_to_301_redirects_options_page_html() {
 
 			// Output security fields
 			settings_fields( 'csv-to-301-redirects' );
-	
+
 			// Output sections and fields
 			do_settings_sections( 'csv-to-301-redirects' );
 
 			// Check file
-			if ( false !== $file_path && false === $file && true !== $redirects || false !== $file_path && false !== $file && false !== $redirects ) { 
-	
+			if ( false !== $file_path && false === $file && true !== $redirects || false !== $file_path && false !== $file && false !== $redirects ) {
+
 				// Get attachment ID
 				$attachment_id = (int) get_option( 'csv_to_301_redirects_csv_file_path' )['csv_to_301_redirects_csv_file_path'];
-				
+
 				// Show attachment info
 				echo '<table class="form-table"><tbody>';
 				echo '<tr>';
@@ -321,12 +327,16 @@ function csv_to_301_redirects_options_page_html() {
 				echo '</tr>';
 				echo '<tr>';
 				echo '<th>Attachment mime: </th>';
-				echo '<td>' . esc_html( mime_content_type( $file_path ) ) . '</td>';
+				if ( false !== $file ) {
+					echo '<td>' . esc_html( mime_content_type( $file_path ) ) . '</td>';
+				} else {
+					echo '<td>' . esc_html__( 'Cannot read file!', 'csv-to-301-redirects' ) . '</td>';
+				}
 				echo '</tr>';
 				echo '</tbody></table>';
-				
+
 			}
-	
+
 			// Output submit button
 			submit_button( 'Save' );
 
@@ -337,11 +347,11 @@ function csv_to_301_redirects_options_page_html() {
     <?php
 
 	// Check
-	if ( false !== $file_path && false !== $file && false !== $redirects ) { 
-	
+	if ( false !== $file_path && false !== $file && false !== $redirects ) {
+
 		// Variables
 		$counter = 0;
-		
+
 	?>
 
 		<div class="wrap">
@@ -352,51 +362,51 @@ function csv_to_301_redirects_options_page_html() {
 						<td style="padding-left: 20px;"><strong>Target</strong></td>
 					</tr>
 				</thead>
-				<tbody>	
+				<tbody>
 					<?php
-		
+
 						// Loop
 						foreach( $redirects as $source => $target ) {
-					
+
 							// Up
 							$counter++;
 
 							// Stop at 250
-							if ( $counter < 250 ) { 
-							
+							if ( $counter < 250 ) {
+
 								?>
-					
+
 								<tr>
 									<td><a href="<?php echo esc_url( home_url() . $source ); ?>" target="blank"><?php echo $source; ?></a></td>
 									<td style="padding-left: 20px;"><?php echo esc_html( $target ); ?></td>
 								</tr>
-					
+
 								<?php
-							
+
 							} else {
-								
+
 								?>
 								<tr><td>-</td></tr>
 								<tr><td><strong>Showing only the first 250 results of the .csv file.</strong></td></tr>
 								<tr><td>-</td></tr>
 								<?php
-								
+
 								// Stop
 								break;
-								
+
 							}
-						
+
 						}
-					
+
 					?>
 				</tbody>
 			</table>
 		</div>
 
 	<?php
-	
+
 	}
-		
+
 }
 
 ?>
